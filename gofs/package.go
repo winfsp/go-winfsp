050c59b70c7b8a12cf1e0ffc50766063d064ea49
aegistudio <www@aegistudio.net>
2026-02-23 18:28:39 +0000

    doc(gofs): expected behavior of case-insensitive filesystem.

--- gofs/package.go
+++ gofs/package.go
@@ -10,6 +10,14 @@
 // On the filesystem level, it supports Stat, OpenFile,
 // Mkdir, Remove and Rename operations.
 //
+// The filesystem can be either case-sensitive or
+// case-insensitive. By default, it's case-sensitive.
+// It can be switched to case-insensitive by specifying
+// `gofs.WithCaseInsensitive(true)`. When it is
+// case-insensitive, the implementor must support
+// opening file with ignored cases, while preserving
+// the cases when the file was created.
+//
 // This makes it works even if the underlying file system
 // is backed by a Window's native directory through the
 // language interfaces by Golang.
